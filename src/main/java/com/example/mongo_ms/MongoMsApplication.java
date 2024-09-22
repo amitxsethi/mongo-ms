da5b2195d35a3987ffe9532703fa7d925f5f8585
amitsethi <amit.x.sethi@gmail.com>
2024-09-22 16:53:11 +0100

    first commit

---
+++ src/main/java/com/example/mongo_ms/MongoMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongo_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongo_ms/MongoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongo_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
